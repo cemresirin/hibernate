51fb8c8861a33d404ac13817fe99b5190bce33ee
cemresirin <cemre25355@gmail.com>
2025-07-20 19:08:47 +0300

    Transaction rollback test edildi

--- src/main/java/com/cemre/Main.java
+++ src/main/java/com/cemre/Main.java
@@ -17,35 +17,40 @@ public class Main {
         //   Student student = new Student("Cemre" , "Şirin");;
         //   session.persist(student);
 
+             //rollback deneme
+        //   Student student2 =null;
+        //    System.out.println(student2.getName());
+
+
             //READ
-            Student student=session.get(Student.class,1);
-            if (student !=null) {
-                System.out.println("Öğrenci adı:"+ student.getName());
-            }else {
-                System.out.println("Öğrenci bulunamadı");
-            }
+        //    Student student=session.get(Student.class,1);
+        //    if (student !=null) {
+        //        System.out.println("Öğrenci adı:"+ student.getName());
+        //    }else {
+        //        System.out.println("Öğrenci bulunamadı");
+        //    }
 
             //UPDATE
-            Student studentUpdate =session.get(Student.class,1);
-            if (studentUpdate !=null) {
-                studentUpdate.setName("Ceylin" );
-                studentUpdate.setLastName("Bal");
-                session.update(studentUpdate);
-            }
+        //    Student studentUpdate =session.get(Student.class,1);
+        //    if (studentUpdate !=null) {
+        //        studentUpdate.setName("Ceylin" );
+        //        studentUpdate.setLastName("Bal");
+        //        session.update(studentUpdate);
+        //    }
 
             //DELETE
-            Student studentDelete = session.get(Student.class,2);
-            if (studentDelete !=null) {
-                session.delete(studentDelete);
-                System.out.println("Öğrenci silindi:" +studentDelete.getName());
-            }else {
-                System.out.println("Silinecek öğrenci bulunamadı");
-            }
+        //    Student studentDelete = session.get(Student.class,2);
+        //    if (studentDelete !=null) {
+        //        session.delete(studentDelete);
+        //        System.out.println("Öğrenci silindi:" +studentDelete.getName());
+        //    }else {
+        //        System.out.println("Silinecek öğrenci bulunamadı");
+        //    }
 
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) transaction.rollback();
-            e.printStackTrace();
+            System.out.println("Rollback çalıştı");
         } finally {
             session.close();
             HibernateUtil.shutdown();
